91e16b8f1a79a7cf8847f49d383eb1f046b5dd23
archercreat <archercreat@gmail.com>
2023-03-18 00:19:47 +0000

    remove useless files

--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-nix-shell -p bundler -p bundix --run 'bundler update; bundler lock; bundler package --no-install --path vendor; bundix; rm -rf vendor'
--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "PogChamp"
-git push
-
-# save blog backup
-7z a blog_backup.7z *
-mv blog_backup.7z /mnt/c/Users/Archer/iCloudDrive/
